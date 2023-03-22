9f038884f9cfd69ea87aaa28ece9f604f6b6120e
Sandesh <khatiwadasandesh01@gmail.com>
2023-03-22 18:52:34 +0545

    Add files via upload

---
+++ Source/script.js
@@ -0,0 +1,24 @@
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        // console.log(entry);
+
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+
+hiddenElements.forEach((el)=>{
+    observer.observe(el);
+})
+
+
+window.addEventListener("scroll",()=>{
+
+    console.log(window.scrollY);
+    if(window.scrollY>=420){
+                //about section
+    }
+})
